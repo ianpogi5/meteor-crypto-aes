f484e6ed0b8dd77e719af281d7bc0f7079966215
Melchor Bungalso <melchor@kayweb.com.au>
2015-01-20 17:34:11 +0800

    Fix compatibility to meteor 1.x

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
-  summary: "AES Package for Crypto.js, standard secure algorithms"
+    name: "ianpogi5:crypto-aes",
+  summary: "AES Package for Crypto.js, standard secure algorithms KAYWEB",
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
 });
 
-Package.on_use(function (api) {
-  api.use('crypto-base',    ['client', 'server']);
-  api.imply('crypto-base',  ['client', 'server']);
-  api.add_files(['evpkdf.js', 'cipher-core.js','aes.js'], ['client', 'server']);
+Package.onUse(function (api) {
+  api.versionsFrom('1.0.2.1');
+  api.use('ianpogi5:crypto-base',    ['client', 'server']);
+  api.imply('ianpogi5:crypto-base',  ['client', 'server']);
+  api.addFiles(['evpkdf.js', 'cipher-core.js','aes.js'], ['client', 'server']);
 });
